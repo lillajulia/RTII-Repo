8865431ce939da033453e98ff42b78fd62b6bd1c
lillajulia <ltath18@student,aau.dk>
2021-06-07 15:37:19 +0200

    UI changes, Octave Button

--- RTII-ColorMusic/Assets/MainScript.cs
+++ RTII-ColorMusic/Assets/MainScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -8,21 +9,23 @@ using UnityEngine.EventSystems;
 public class MainScript : MonoBehaviour
 
 {
+    public bool isMute=false;
     
-    
-    
-    private float sampling_frequency = 48000;
-    
+    public Button octaveButton;
+    public Button muteButton;
+    public Sprite EnabledMuteSprite;
+    public Sprite DisabledMuteSprite;
 
+    private int octave = 1;
+
+    [CanBeNull] private AudioListener _audioListener;
+    
+    
+    private float samplingFrequency = 48000;
     public float frequency;
     public float gain = 0.05f;
     private float increment;
     private float phase;
-  
-    
-    
-    
-    
 
 
     private bool IsCameraAvaliable;
@@ -36,115 +39,91 @@ public class MainScript : MonoBehaviour
 
     public int CenterX = Screen.width / 2;
     public int CenterY = Screen.height / 2;
+
     
-    
-    public float scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue){
- 
-        float OldRange = (OldMax - OldMin);
-        float NewRange = (NewMax - NewMin);
-        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
- 
-        return(NewValue);
-    }
-   
     void OnAudioFilterRead(float[] data, int channels)
     {
-        float tonalPart = 0;
+        float tone = 0;
         
-        // update increment in case frequency has changed
-        increment = frequency * 2f * Mathf.PI / sampling_frequency;
+        increment = frequency * 2f * Mathf.PI / samplingFrequency;
 
         for (int i = 0; i < data.Length; i++)
         {
-            
-            
-
             phase = phase + increment;
             if (phase > 2 * Mathf.PI) phase = 0;
             
-            //tone
-            tonalPart = (1f ) * (float)(gain * Mathf.Sin(phase));
-            
-            data[i] = tonalPart;
+            tone = (1f) * (float) (gain * Mathf.Sin(phase));
 
-            // if we have stereo, we copy the mono data to each channel
+            data[i] = tone;
             if (channels == 2)
             {
                 data[i + 1] = data[i];
                 i++;
             }
-
-            
         }
 
         void Awake()
         {
-            sampling_frequency = AudioSettings.outputSampleRate;
-            Update();
+            
         }
-        
-        
     }
+
     void Start()
     {
-       
-      
-        defaultBackground = background.texture;
-        WebCamDevice[] devices = WebCamTexture.devices;
+        octaveButton.onClick.AddListener(octaveSwitch);
 
-        if (devices.Length == 0)
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices; // start looking for available cameras on the device 
+
+        if (devices.Length == 0) // if devices.length is zero, it means there are no available cameras on the device
         {
-            Debug.Log("No camera");
-            IsCameraAvaliable = false;
-            return;
+            Debug.Log("No camera"); // if there are no available cameras, we print out No Camera
+            IsCameraAvaliable = false; // We also set the IsCameraAvailable boolean to false, because there is no cam available. Duh. 
+            return; // if there is no camera, the program should not go further
         }
 
-        for (int i = 0; i < devices.Length; i++)
+        for (int i = 0; i < devices.Length; i++) // We go through all available cameras on the device, and select the one that is not front facing
         {
-            if (!devices[i].isFrontFacing)
+            if (!devices[i].isFrontFacing) // if not front facing camera then....
             {
-                PhoneCamera = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+                PhoneCamera = new WebCamTexture(devices[i].name, Screen.width, Screen.height); //...set the device's PhoneCamera texture to the incoming video input
             }
         }
 
-        if (PhoneCamera == null)
+        if (PhoneCamera == null) //if we couldn't find a back facing camera,we should display No camera, and not proceed further
         {
             Debug.Log("No camera");
             return;
-
         }
 
-        PhoneCamera.Play();
-        background.texture = PhoneCamera;
+        PhoneCamera.Play(); 
+        background.texture = PhoneCamera;  // render the incoming video to the background 
         IsCameraAvaliable = true;
-        
-        
     }
 
 
     void Update()
     {
-
+        _audioListener = GetComponent<AudioListener>();
         if (!IsCameraAvaliable)
         {
             return;
         }
 
-        float ratio = (float) PhoneCamera.width / (float) PhoneCamera.height;
-        fit.aspectRatio = ratio;
+        float ratio = (float) PhoneCamera.width / (float) PhoneCamera.height; // calculate aspect ratio
+        fit.aspectRatio = ratio; //set the aspect ratio
 
-        float scaleY = PhoneCamera.videoVerticallyMirrored ? -1f : 1f;
+        float scaleY = PhoneCamera.videoVerticallyMirrored ? -1f : 1f; 
         background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
         int orient = -PhoneCamera.videoRotationAngle;
 
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
 
-        PhoneCamera.GetPixel(CenterX, CenterY);
-
         float RedAvg = 0;
         float GreenAvg = 0;
         float BlueAvg = 0;
+        
         for (int i = -20; i <= 20; i++)
         {
             for (int j = -20; j <= 20; j++)
@@ -155,71 +134,84 @@ public class MainScript : MonoBehaviour
             }
         }
 
-        RedAvg = (RedAvg / (41 * 41)) * 255;
-        GreenAvg = (GreenAvg / (41 * 41)) * 255;
-        BlueAvg = (BlueAvg / (41 * 41)) * 255;
+        RedAvg = (RedAvg / (41 * 41));
+        GreenAvg = (GreenAvg / (41 * 41));
+        BlueAvg = (BlueAvg / (41 * 41));
 
-       
+        float H, S, V;
 
+        Color.RGBToHSV(new Color(RedAvg, GreenAvg, BlueAvg, 1), out H, out S, out V);
 
-        //float Red = PhoneCamera.GetPixel(CenterX,CenterY).r;
-        //float Green = PhoneCamera.GetPixel(CenterX,CenterY).g; 
-        //float Blue = PhoneCamera.GetPixel(CenterX,CenterY).b; 
-
-        //print(RedAvg);
-        //print(BlueAvg);
-        //print(GreenAvg);
-        //float combined = RedAvg + GreenAvg + BlueAvg;
-
-        //frequency=scale(0, 3, 440, 4000, combined);
-        //print(scale(0, 3, 440, 4000, combined));
-        // print("RED"); 
-        // print(RedAvg);
-        // print("GREEN"); 
-        // print(GreenAvg);
-        // print("BLUE"); 
-        // print(BlueAvg);
-
-
-        if (RedAvg <= 80  && GreenAvg <= 80 && BlueAvg <= 80)
+        H = H * 360;
+        S = S * 360;
+        V = V * 360;
+        
+        if (S <= 40 && V <= 60)
         {
-            frequency = 349.2f;
-            print("F");
+            frequency = 261.626f;
+            print("C4");
         }
-        else if (RedAvg >= 170 && GreenAvg <= 75 && BlueAvg <= 75)
+        else if (H <= 30 && S >= 40 && V >= 40)
         {
-            frequency = 392;
-            print("G");
+            frequency = 293.665f;
+            print("D4");
         }
-        else if (RedAvg >= 170 && GreenAvg >= 170  && BlueAvg <= 75)
+        else if (H >= 31 && H <= 70 && S >= 40 && V >= 40)
+        {
+            frequency = 329.628f;
+            print("E4");
+        }
+        else if (H >= 71 && H <= 150 && S >= 40 && V >= 40)
+        {
+            frequency = 349.228f;
+            print("F4");
+        }
+        else if (H >= 151 && H <= 240 && S >= 40 && V >= 40)
+        {
+            frequency = 391.995f;
+            print("G4");
+        }
+        else if (H >= 241 && H <= 330 && S >= 40 && V >= 40)
         {
             frequency = 440;
-            print("A");
+            print("A4");
         }
-        else if (RedAvg <= 75 && GreenAvg >= 170 && BlueAvg <= 75)
+        else if (H >= 331 && H <= 360 && S >= 40 && V >= 40)
         {
-            frequency = 493.9f;
-            print("B");
+            frequency = 493.883f;
+            print("B4");
         }
-        else if (RedAvg <= 75 && GreenAvg <= 75 && BlueAvg >= 170)
-        { 
-            frequency = 523.3f;
-            print("C");
-        }
-        else if (RedAvg >= 170 && GreenAvg <= 75 && BlueAvg >= 170)
-        { 
-            frequency = 587.3f;
-            print("D");
-        }
-        else if (RedAvg >= 170 && GreenAvg >= 170 && BlueAvg >= 170)
-        { 
-            frequency = 659.3f;
-            print("E");
-        }
-        
-        
 
-
+        frequency = frequency * octave;
 
     }
+
+    public void octaveSwitch()
+    {
+        if (octave < 4)
+        {
+            octave++;
+        }
+        else
+        {
+            octave = 1;
+        }
     }
+    public void mute()
+    {
+        if (!isMute)
+        {
+            
+            isMute = true;
+            gain = 0;
+            muteButton.image.sprite = DisabledMuteSprite;
+        }
+        else
+        {
+            isMute = false;
+            gain = 0.05f; 
+            muteButton.image.sprite = EnabledMuteSprite;
+        }
+
+    }
+}
